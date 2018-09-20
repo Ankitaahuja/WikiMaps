d2f325b5524c54d45f41769699f79c1028c3ea99
paulabarreto <paulavilaca@gmail.com>
2018-09-20 15:44:10 +0000

    seeds

---
+++ db/seeds/2maps.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  return knex('maps').del()
+    .then(function () {
+      return Promise.all([
+        knex('maps').insert({id: 1, map_name: 'Toronto', latitude: 4000, longitude: 9000, zoomlevel:3,user_id: 1 }),
+        knex('maps').insert({id: 2, map_name: 'Vancouber', latitude: 60000, longitude: 18000, zoomlevel:5,user_id: 2 }),
+        knex('maps').insert({id: 3, map_name: 'Ottawa', latitude: 70000, longitude: 6000, zoomlevel:7,user_id: 3 })
+      ]);
+    });
+};
+
+
+
