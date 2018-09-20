ca4ad011157416e25a76694dd7347c77c48c6e66
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-09-20 01:21:42 +0000

    Updated the routes in server.js

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ const knexConfig = require("./knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 const morgan = require('morgan');
 const knexLogger = require('knex-logger');
+const cookieSession = require('cookie-session');
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['yama'],
+    maxAge: 24 * 60 * 60 * 1000 // Cookie Options, 24 hours
+  }));
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+//const usersRoutes = require("./routes/users");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -23,7 +30,7 @@ const usersRoutes = require("./routes/users");
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+//app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -38,7 +45,7 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+//app.use("/api/users", usersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
@@ -51,18 +58,36 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body.email);
-  const user = {
-    email: req.body.email,
-    password: req.body.password,
-    user_name: req.body.username
-  };
-  console.log(user);
-  knex("users").insert(user)
-  .returning ('*')
-  .then(rows => console.log('success!', rows))
-  .catch(err => console.log('error!', err.message))
-  res.redirect("/login");
+
+        knex('users')
+        .where({email:req.body.email})
+        .then(function(rows) {
+            if(rows.length>0){
+              res.send('Sorry User Already exist with the email: '+req.body.email)
+            }else{
+
+              const user = {
+                email: req.body.email,
+                password: req.body.password,
+                user_name: req.body.username
+              };
+
+              knex("users")
+              .insert(user)
+              .then(function(){
+                res.redirect("/login");
+              })
+              .catch(function(error){
+                res.send('Error Occurred, Please check email and try again later '+error.message);
+              })
+    
+            }
+        })
+        .catch(function(error) {
+            console.error(error)
+            res.send('Error Occurred '+error)
+        });
+
 })
 
 // Login page
@@ -72,14 +97,31 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
 
-  knex('users')
-  .then(function(rows) {
-    if(req.body.email && req.body.password){
-     res.redirect("/");
-    }
-    })
-  .catch(err => console.log('error!', err.message))
+        knex('users')
+        .where({email:req.body.email})
+        .then(function(rows) {
+            if(rows.length>0){
+                console.log(JSON.stringify(rows[0]))
+                if(req.body.password === rows[0].password){
+                    console.log('Password Matches')
+                    res.redirect('/')
+                }else{
+                    console.log('Password fails')
+                    res.send('Sorry Incorrect Password, Please try again!')
+                }
+            }else{
+                res.send('No User Found with the email: '+req.body.email)
+            }
+        })
+        .catch(function(error) {
+            console.error(error)
+            res.send('Error Occurred '+error)
+        });
+
 });
+
+
+
 
 
 app.listen(PORT, () => {
