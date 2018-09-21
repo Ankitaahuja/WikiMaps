730fa22f4c53b4de898f49fcd552d298535258e2
kkwon39 <kkwon39@gmail.com>
2018-09-21 17:34:10 +0000

    added attributes to tables

--- db/migrations/20180919203708_create_favorites_table.js
+++ db/migrations/20180919203708_create_favorites_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('favorites', function(table) {
             //table.increments('id');, add this, drop old table and then new table
+            table.increments('id');
             table.integer('user_id').unsigned();
             table.foreign('user_id').references("id").inTable("users");
             table.integer('map_id').unsigned();
--- db/seeds/4favorites.js
+++ db/seeds/4favorites.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('favorites').del()
     .then(function () {
       return Promise.all([
-        knex('favorites').insert({user_id: 1, map_id: 1}),
-        knex('favorites').insert({user_id: 2, map_id: 2}),
-        knex('favorites').insert({user_id: 3, map_id: 3})
+        knex('favorites').insert({id: 1, user_id: 1, map_id: 1}),
+        knex('favorites').insert({id: 2, user_id: 2, map_id: 2}),
+        knex('favorites').insert({id: 3, user_id: 3, map_id: 3})
       ]);
     });
 };
